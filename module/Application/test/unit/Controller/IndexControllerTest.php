6c4d5ac619e1ee217fc87093daf7b2b1df34974c
Eduardo Marques <eduardomarqs1@gmail.com>
2020-09-13 18:38:05 +0200

    #9: fix failing tests; fix autoload in composer;

--- module/Application/test/unit/Controller/IndexControllerTest.php
+++ module/Application/test/unit/Controller/IndexControllerTest.php
@@ -4,15 +4,16 @@ declare(strict_types=1);
 namespace ApplicationUnitTest\Controller;
 
 use Application\Controller\IndexController;
+use ApplicationUnitTest\AbstractControllerUnitTestCase;
 use Laminas\I18n\Translator\TranslatorInterface;
+use Laminas\Mvc\Controller\Plugin\Url;
 use Laminas\View\Model\ViewModel;
 use PHPUnit\Framework\MockObject\MockObject;
-use PHPUnit\Framework\TestCase;
 
 /**
  * @group Unit
  */
-final class IndexControllerTest extends TestCase
+final class IndexControllerTest extends AbstractControllerUnitTestCase
 {
     /**
      * @var TranslatorInterface|MockObject
@@ -21,34 +22,46 @@ final class IndexControllerTest extends TestCase
 
     private IndexController $indexController;
 
+    /**
+     * @var Url|MockObject
+     */
+    private MockObject $url;
+
+
     protected function setUp(): void
     {
         parent::setUp();
-        $this->translator = $this->createMock(TranslatorInterface::class);
+        $this->translator      = $this->createMock(TranslatorInterface::class);
         $this->indexController = new IndexController($this->translator);
+
+        $this->url = $this->createMockForUrlPlugin($this->indexController);
     }
+
 
     public function testIndexAction(): void
     {
         $this->translator->expects(static::exactly(3))
-            ->method('translate')
-            ->withConsecutive(
-                ['welcome_message'],
-                ['description_message'],
-                ['new_contract_button_label']
-            )
-            ->willReturnArgument(0);
+                         ->method('translate')
+                         ->withConsecutive(
+                             ['welcome_message'],
+                             ['description_message'],
+                             ['new_contract_button_label']
+                         )
+                         ->willReturnArgument(0);
+
+        $this->url->expects(static::once())
+                  ->method('fromRoute')
+                  ->with('new-contract')
+                  ->willReturn('new/contract/link');
 
         $expectedVariables = [
             'welcomeMessage'         => 'welcome_message',
             'descriptionMessage'     => 'description_message',
             'newContractButtonLabel' => 'new_contract_button_label',
-            'newContractLink'        => '',
+            'newContractLink'        => 'new/contract/link',
         ];
 
-        $expected = (new ViewModel())->setVariables($expectedVariables);
-
-        static::assertEquals($expected, $this->indexController->indexAction());
+        static::assertEquals(new ViewModel($expectedVariables), $this->indexController->indexAction());
     }
 
 }
